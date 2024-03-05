7814b14262dc8b74613a8c824c8e9e4e894820eb
gauravsars <gauravsars11@gmail.com>
2024-03-06 00:59:58 +0530

    Commit-Backend-001 : Upto First SpringBoot API for createEmployee functionality.

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package net.javaguides.springboot.service;
+
+import net.javaguides.springboot.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto) ;
+
+
+
+
+}
---
+++ springboot-backend/src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot.service.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import net.javaguides.springboot.dto.EmployeeDto;
+import net.javaguides.springboot.entity.Employee;
+import net.javaguides.springboot.mapper.EmployeeMapper;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import net.javaguides.springboot.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+}
